672a77ac26f29646885a88fa8147b7608510674b
fortune <fongchun@amazon.com>
2024-06-28 02:46:46 +0800

    deploy script for frontend

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,5 +1,6 @@
 STACK_NAME="fgac-frontend"
 FRONTEND_REPOSITORY="https://github.com/ShinyTopology/test-rag-os-fgac"
+SECRET_NAME_GITHUB_TOKEN=FrontendGithubToken
 
 # Check if the stack exists
 stack_exists=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].StackName" --output text 2>/dev/null)
@@ -11,8 +12,8 @@ if [ "$stack_exists" == "$STACK_NAME" ]; then
 else
   aws cloudformation create-stack \
     --stack-name $STACK_NAME \
-    --template-body file://frontend.yaml \
+    --template-body file://iac/frontend.yaml \
     --parameters ParameterKey=FrontendRepository,ParameterValue=$FRONTEND_REPOSITORY \
-                 ParameterKey=GitHubTokenSecretName,ParameterValue=FrontendGithubToken \
+                 ParameterKey=GitHubTokenSecretName,ParameterValue=$SECRET_NAME_GITHUB_TOKEN \
     --capabilities CAPABILITY_NAMED_IAM
 fi
